b863a0fd4d52fbe8569a8b665038e21f776bc696
NowhereToRun <zihao5@staff.sina.com.cn>
2018-02-09 16:35:12 +0800

    init

---
+++ src/js/tools.js
@@ -0,0 +1,38 @@
+import './lib/zepto'
+
+class statusTools {
+  constructor() {
+    this.wrapper = null;
+    this.allItems = {}
+    this.init();
+  }
+  init() {
+    let wrapper = $('#j_status_tools');
+    if (!wrapper.length) {
+      this.wrapper = $('<div id="j_status_tools" style="position:fixed; top:0; right:0; max-height:200px;min-height:50px; width:200px; background:rgba(45, 67, 72, 0.8);padding: 0 5px;z-index: 9999;"></div>');
+      $(document.body).append(this.wrapper);
+    } else {
+      this.wrapper = wrapper;
+    }
+  }
+  addItem(id, msg) {
+    let selector = 'j_status_p___' + id;
+    if (!$('#' + selector).length) {
+      let item = $('<p id="j_status_p___' + id + '" style="color:#0cd8d4;font-size: 13px;">' + id + ': <span class="j_' + id + '"></span></p>');
+      this.wrapper.append(item);
+      this.allItems[id] = item;
+      if (msg != null) {
+        this.allItems[id].find('.j_' + id).text(msg);
+      }
+    } else {
+      this.updateItem(id, msg);
+    }
+  }
+  updateItem(id, msg) {
+    if (this.allItems[id] && msg != null) {
+      this.allItems[id].find('.j_' + id).text(msg);
+    }
+  }
+}
+
+export default statusTools
--- src/view/scroll/index.js
+++ src/view/scroll/index.js
@@ -1,10 +1,40 @@
-document.write("当你看到这句话 你的环境已经搭建成功");
-import style from "../../css/index.css"
+import tools from '../../js/tools'
+var statusPanel = new tools();
+var lastTime = new Date();
 
-console.log(1);
+function showScrollTop() {
+  statusPanel.addItem('touchmove', document.body.scrollTop || document.documentElement.scrollTop);
+}
 
-require.ensure(["./1"], function(require) {
-    var module2 = require("./1");
-    console.log(1)
-    require("./2");
-}, 'test');
+window.addEventListener('scroll', function() {
+  statusPanel.addItem('scrollTop', document.body.scrollTop || document.documentElement.scrollTop);
+});
+
+var pageMain = document.querySelector('.page_main');
+pageMain.addEventListener('touchmove', showScrollTop);
+
+function showTime() {
+  let time = new Date();
+  let hours = time.getHours();
+  let minus = time.getMinutes();
+  let secs = time.getSeconds();
+  if (hours >= 0 && hours <= 9) {
+    hours = "0" + hours;
+  }
+  if (minus >= 0 && minus <= 9) {
+    minus = "0" + minus;
+  }
+  if (secs >= 0 && secs <= 9) {
+    secs = "0" + secs;
+  }
+  statusPanel.addItem('time', hours + ':' + minus + ':' + secs);
+
+  setTimeout(() => {
+    showTime();
+    $.ajax({
+      url: 'http://10.222.22.54:8888'
+    })
+  }, 1000);
+}
+
+showTime();
